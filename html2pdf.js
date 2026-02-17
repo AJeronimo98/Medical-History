34325d0366244a48a2b4475c52f7fb8be543c0b5
AJeronimo98 <angeljeronimo121@gmail.com>
2026-02-17 17:03:24 -0600

    demo version 3

--- html2pdf.js
+++ html2pdf.js
@@ -1,13 +1,13 @@
-function descargarPDF(){
-  const elemento = document.querySelector(".pagina"); // Todo el contenido
+function descargarPDF() {
+  const elemento = document.querySelector(".pagina"); // Contenido completo
 
   const opciones = {
-    margin: 10,               // margen en mm
+    margin: 10,               
     filename: 'Historia_Clinica.pdf',
     image: { type: 'jpeg', quality: 1 },
     html2canvas: { scale: 2, useCORS: true },
     jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' },
-    pagebreak: { mode: ['css', 'legacy'] } // rompe en secciones largas
+    pagebreak: { mode: ['css', 'legacy'] } // rompe páginas automáticamente
   };
 
   html2pdf().set(opciones).from(elemento).save();
