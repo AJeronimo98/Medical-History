49d3d1f9ee58751cfe348d6db589109e8481b173
AJeronimo98 <angeljeronimo121@gmail.com>
2026-02-18 11:15:56 -0600

    Pdf descarga

--- html2pdf.js
+++ html2pdf.js
@@ -2,10 +2,16 @@ function descargarPDF() {
   const elemento = document.querySelector(".pagina"); // Contenido completo
 
   const opciones = {
-    margin: 10,               
+    margin: [10, 0, 10, 0], // margen superior, derecho, inferior, izquierdo
     filename: 'Historia_Clinica.pdf',
     image: { type: 'jpeg', quality: 1 },
-    html2canvas: { scale: 2, useCORS: true },
+    html2canvas: { 
+      scale: 3,       // mayor resolución
+      useCORS: true, 
+      scrollX: 0,     // asegura captura desde esquina superior izquierda
+      scrollY: 0,
+      logging: true   // opcional: útil para depurar
+    },
     jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' },
     pagebreak: { mode: ['css', 'legacy'] } // rompe páginas automáticamente
   };
